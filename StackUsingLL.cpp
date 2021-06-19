068b80b7f357901b8303378ed20f0bf303f9e9ce
onkarr19 <onkarchendage19@gmail.com>
2021-06-20 00:13:33 +0530

    stack with classes

--- StackUsingLL.cpp
+++ StackUsingLL.cpp
@@ -1,55 +1,86 @@
 #include <iostream>
 using namespace std;
-#define size 10
+#define get(a) int a; cin >> a;
+#define println(a) cout << a << "\n";
+#define print(a) cout << a << " ";
 
-class stack {
-private:
-	int top;
-	int s[size];
+class Node{
+public:
+	int data;
+	Node* next;
+	Node() {next=NULL;}
+};
 
-	bool isFull() {
-		return top == size-1;
+
+class Stack{
+	Node* top;
+public:
+	Stack() {top=NULL;}
+
+	void push(int k) {
+		Node* temp = new Node();
+		if (temp) {
+			temp->data = k;
+			temp->next = top;
+			top = temp;
+		} else println("Stack Overflow");
 	}
 
 	bool isEmpty() {
-		return top == -1;
-	}
-
-public:
-	void push(int m) {
-		if (!isFull())
-		{
-			s[++top] = m;
-		} else cout << "Stack Overflow\n";
-	}
-
-	int pop() {
-		int val = -1;
-		if (!isEmpty())
-		{
-			val = s[top];
-			top--;
-		} else cout << "Stack Underflow\n";
-		return val;
+		return top == NULL;
 	}
 
 	int peek() {
-		return s[top];
+		return isEmpty() ? -1 : top->data;
+	}
+
+	int pop() {
+		if(isEmpty()) {
+			println("Stack Underflow");
+			return -1;
+		} else {
+			int val = top->data;
+			Node* temp = top;
+			top = top->next;
+			temp->next = NULL;
+			free(temp);
+			return val;
+		}
+	}
+
+	void printStack() {
+		println("Stack elements:");
+		if (isEmpty())
+		{
+			println("Stack Underflow");
+			return;
+		}
+		Node* temp = top;
+		while(temp!=NULL) {
+			println(temp->data);
+			temp = temp->next;
+		} println(" ");
 	}
 };
 
-int main() {
-	
-	stack s;
-	s.push(1);
-	s.push(2);
-	s.push(14);
-	s.push(13);
-	s.push(19);
-	s.push(12);
 
+int main() {
+	Stack s;
+	s.push(23);
+	s.push(43);
+	s.push(89);
+	s.push(53);
+	s.printStack();
+	println(s.peek());
+
+	println("Popping: ");
+
+	// cout << s.pop() << endl;
 	cout << s.pop() << endl;
-	cout << s.pop() << endl;
-	cout << s.pop() << endl;
+	// cout << s.pop() << endl;
+	// cout << s.pop() << endl;
+
+	s.printStack();
+	println(s.peek());
 	return 0;
 }
